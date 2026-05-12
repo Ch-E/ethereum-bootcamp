aa1a1479d4b8bc3e2b8c3ee0040011f8b122cc4a
Ch-E <charlesterhuysen303@gmail.com>
2026-05-12 06:53:05 -0500

    Fix cryptographic-hashes exercise and add npm test setup

--- blockchain-cryptography/cryptographic-hashes/index.js
+++ blockchain-cryptography/cryptographic-hashes/index.js
@@ -6,7 +6,16 @@ const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
 // given a hash, return the color that created the hash
 function findColor(hash) {
-    
+    const hashHex = typeof hash === 'string' ? hash : toHex(hash);
+
+    for (const color of COLORS) {
+        const colorHash = sha256(utf8ToBytes(color));
+        if (toHex(colorHash) === hashHex) {
+            return color;
+        }
+    }
+
+    return "no color found for provided hash";
 }
 
 module.exports = findColor;
