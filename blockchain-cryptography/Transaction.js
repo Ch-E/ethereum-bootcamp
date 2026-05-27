d9e605fd979a29cac342f94b6bd7b544751534ce
Ch-E <charlesterhuysen303@gmail.com>
2026-05-27 06:36:56 -0500

    Verify UTXO amounts

--- blockchain-cryptography/Transaction.js
+++ blockchain-cryptography/Transaction.js
@@ -1,10 +1,26 @@
+const TXO = require("./TXO");
+
 class Transaction {
     constructor(inputUTXOs, outputUTXOs) {
         this.inputUTXOs = inputUTXOs
         this.outputUTXOs = outputUTXOs
     }
     execute() {
-        
+        for (let i = 0; i < this.inputUTXOs.length; i++) {
+            if (this.inputUTXOs[i].spent) {
+                throw new Error('UTXO already spent')
+            }
+        }
+
+        const inputAmount = this.inputUTXOs.reduce((p, c) => {
+            return p + c.amount;
+        }, 0);
+        const outputAmount = this.outputUTXOs.reduce((p, c) => {
+            return p + c.amount;
+        }, 0);
+        if (inputAmount < outputAmount) {
+            throw new Error("Not enough here");
+        }
     }
 }
 
