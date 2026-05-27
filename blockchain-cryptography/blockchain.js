07adf0e6e88e678ff8b97367be08512f25142b1a
Ch-E <charlesterhuysen303@gmail.com>
2026-05-27 06:12:47 -0500

    TXO

--- blockchain-cryptography/blockchain.js
+++ blockchain-cryptography/blockchain.js
@@ -10,6 +10,18 @@ class Blockchain {
         block.previousHash = this.chain.at(-1).toHash();
         this.chain.push(block)
     }
+
+    isValid() {
+        for(let i = this.chain.length - 1; i > 0; i--) {
+            const block = this.chain[i];
+            const prev = this.chain[i - 1];
+            
+            if(block.previousHash.toString() !== prev.toHash().toString()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 module.exports = Blockchain;
