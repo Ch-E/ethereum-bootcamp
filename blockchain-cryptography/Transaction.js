c6082a490a860b53b5c9acaef4ead1368dacb13f
Ch-E <charlesterhuysen303@gmail.com>
2026-05-27 06:21:46 -0500

    Transaction

---
+++ blockchain-cryptography/Transaction.js
@@ -0,0 +1,11 @@
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs = inputUTXOs
+        this.outputUTXOs = outputUTXOs
+    }
+    execute() {
+        
+    }
+}
+
+module.exports = Transaction;
