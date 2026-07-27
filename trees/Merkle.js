fac2ae04b9239b2430039002b1efa907eb3cfba8
Ch-E <charlesterhuysen303@gmail.com>
2026-07-27 06:19:12 -0500

    getProof

--- trees/Merkle.js
+++ trees/Merkle.js
@@ -3,8 +3,59 @@ class MerkleTree {
         this.leaves = leaves;
         this.concat = concat;
     }
+    getProof(index) {
+        const proof = [];
+        let layer = this.leaves;
+        let currentIndex = index;
+
+        while (layer.length > 1) {
+            const nextLayer = [];
+
+            for (let i = 0; i < layer.length; i += 2) {
+                if (i + 1 >= layer.length) {
+                    nextLayer.push(layer[i]);
+                    if (currentIndex === i) {
+                        currentIndex = Math.floor(i / 2);
+                    }
+                } else {
+                    const left = layer[i];
+                    const right = layer[i + 1];
+
+                    nextLayer.push(this.concat(left, right));
+
+                    if (currentIndex === i) {
+                        proof.push({ data: right, left: false });
+                        currentIndex = Math.floor(i / 2);
+                    } else if (currentIndex === i + 1) {
+                        proof.push({ data: left, left: true });
+                        currentIndex = Math.floor(i / 2);
+                    }
+                }
+            }
+
+            layer = nextLayer;
+        }
+
+        return proof;
+    }
     getRoot() {
-        return this.concat(this.leaves[0], this.leaves[1]);
+        let layer = this.leaves;
+
+        while (layer.length > 1) {
+            const nextLayer = [];
+
+            for (let i = 0; i < layer.length; i += 2) {
+                if (i + 1 >= layer.length) {
+                    nextLayer.push(layer[i]);
+                } else {
+                    nextLayer.push(this.concat(layer[i], layer[i + 1]));
+                }
+            }
+
+            layer = nextLayer;
+        }
+
+        return layer[0];
     }
 }
 
