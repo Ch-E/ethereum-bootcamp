fa2b1aea4badb4156f9544ef7498b637cd03d9f5
Ch-E <charlesterhuysen303@gmail.com>
2026-08-10 06:26:45 -0500

    findEther

---
+++ ethereum/findEther.js
@@ -0,0 +1,27 @@
+const { providers } = require('ethers');
+const { ganacheProvider } = require('./config');
+
+const provider = new providers.Web3Provider(ganacheProvider);
+
+/**
+ * Given an ethereum address find all the addresses
+ * that were sent ether from that address
+ * @param {string} address - The hexadecimal address for the sender
+ * @async
+ * @returns {Array} all the addresses that received ether
+ */
+async function findEther(address) {
+    const addresses = [];
+    const blockNumber = await provider.getBlockNumber();
+    for (let i = 0; i <= blockNumber; i++) {
+        const block = await provider.getBlockWithTransactions(i);
+        block.transactions.forEach((tx) => {
+            if (tx.from === address) {
+                addresses.push(tx.to);
+            }
+        });
+    }
+    return addresses;
+}
+
+module.exports = findEther;
